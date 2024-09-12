814f716e6bf0864a9778c09dbb2e7447cda25eac
koya-T <koya.take.1125@gmail.com>
2024-09-12 15:19:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function setup() {
 }
 
 function draw() {
+    for(let i = 0; i < 22; i++){
+        for(let j = 0; j < 12; j++){
+            // translate();
+            square(10, 10, 10);
+        }
+    }
 }
