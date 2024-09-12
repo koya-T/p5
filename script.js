a4543470f422bd3d0e492c5f053045e6174184fe
koya-T <koya.take.1125@gmail.com>
2024-09-12 15:22:45 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function draw() {
     for(let i = 0; i < 22; i++){
         for(let j = 0; j < 12; j++){
             // translate();
+            fill(255);
             square(10, 10, 10);
         }
     }
